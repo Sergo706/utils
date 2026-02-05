f4a97d0d905da469f8a35eceed98fe2f7577c163
Sergo706 <187537278+Sergo706@users.noreply.github.com>
2026-02-05 15:57:06 +0200

    feat: add fisher-yates shuffle and picsum image utils
    - fisherYatesShuffle: Added unbiased array shuffle utility
    - getRandomImage: Added random image fetcher with fallback seed generation
    - main: Exported new modules

--- main.ts
+++ main.ts
@@ -4,6 +4,8 @@ export * from './generic/configurationDefiner.js';
 export { default as ensureArray } from './generic/ensureArray.js';
 export * from './generic/filterArray.js';
 export * from './generic/findObjectValues.js';
+export * from './generic/fisherYatesShuffle.js';
+export * from './generic/getRandomImage.js';
 export * from './generic/isObjectHasValues.js';
 export * from './generic/miniCache.js';
 export * from './generic/promiseLocker.js';
@@ -11,6 +13,5 @@ export * from './generic/random.js';
 export * from './generic/range.js';
 export * from './generic/safeMerge.js';
 export * from './generic/validateZodSchema.js';
-
 export * from './types/StandardResponse.js';
 export * from './types/utility-types.js';
